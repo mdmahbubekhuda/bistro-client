48343138c8314a24f42abc1a903293edf70f387e
mdmahbubekhuda <md.mahbub.e@gmail.com>
2024-02-06 00:01:42 +0600

    file name changed

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,4 +1,6 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../pages/Shared/Navbar/Navbar";
+import Footer from "../pages/Shared/Footer/Footer";
 
 const Main = () => {
   return (
