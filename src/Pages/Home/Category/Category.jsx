b46488869febf59e48f51f4dcc5bd589bd67f46c
mdmahbubekhuda <md.mahbub.e@gmail.com>
2024-02-05 21:39:35 +0600

    section title component made

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -9,60 +9,68 @@ import slide2 from "../../../assets/home/slide2.jpg";
 import slide3 from "../../../assets/home/slide3.jpg";
 import slide4 from "../../../assets/home/slide4.jpg";
 import slide5 from "../../../assets/home/slide5.jpg";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 const Category = () => {
   return (
-    <Swiper
-      slidesPerView={4}
-      spaceBetween={30}
-      centeredSlides={false}
-      pagination={{
-        clickable: true,
-      }}
-      modules={[Pagination]}
-      className="mySwiper text-center my-8"
-    >
-      <SwiperSlide>
-        <div className="grid">
-          <img src={slide1} className="row-start-1 col-start-1" />
-          <h3 className="text-4xl uppercase row-start-1 col-start-1 self-end mb-4 bg-clip-text text-transparent bg-gradient-to-b from-gray-400 to-gray-700">
-            salad
-          </h3>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="grid">
-          <img src={slide2} className="row-start-1 col-start-1" />
-          <h3 className="text-4xl uppercase row-start-1 col-start-1 self-end mb-4 bg-clip-text text-transparent bg-gradient-to-b from-gray-400 to-gray-700">
-            pizza
-          </h3>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="grid">
-          <img src={slide3} className="row-start-1 col-start-1" />
-          <h3 className="text-4xl uppercase row-start-1 col-start-1 self-end mb-4 bg-clip-text text-transparent bg-gradient-to-b from-gray-400 to-gray-700">
-            soup
-          </h3>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="grid">
-          <img src={slide4} className="row-start-1 col-start-1" />
-          <h3 className="text-4xl uppercase row-start-1 col-start-1 self-end mb-4 bg-clip-text text-transparent bg-gradient-to-b from-gray-400 to-gray-700">
-            desert
-          </h3>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="grid">
-          <img src={slide5} className="row-start-1 col-start-1" />
-          <h3 className="text-4xl uppercase row-start-1 col-start-1 self-end mb-4 bg-clip-text text-transparent bg-gradient-to-b from-gray-400 to-gray-700">
-            salad
-          </h3>
-        </div>
-      </SwiperSlide>
-    </Swiper>
+    <section>
+      <SectionTitle
+        heading={"order online"}
+        subHeading={"From 11.00am to 10.00pm"}
+      />
+
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        centeredSlides={false}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper text-center my-8"
+      >
+        <SwiperSlide>
+          <div className="grid">
+            <img src={slide1} className="row-start-1 col-start-1" />
+            <h3 className="text-4xl uppercase row-start-1 col-start-1 self-end mb-4 bg-clip-text text-transparent bg-gradient-to-b from-gray-400 to-gray-700">
+              salad
+            </h3>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="grid">
+            <img src={slide2} className="row-start-1 col-start-1" />
+            <h3 className="text-4xl uppercase row-start-1 col-start-1 self-end mb-4 bg-clip-text text-transparent bg-gradient-to-b from-gray-400 to-gray-700">
+              pizza
+            </h3>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="grid">
+            <img src={slide3} className="row-start-1 col-start-1" />
+            <h3 className="text-4xl uppercase row-start-1 col-start-1 self-end mb-4 bg-clip-text text-transparent bg-gradient-to-b from-gray-400 to-gray-700">
+              soup
+            </h3>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="grid">
+            <img src={slide4} className="row-start-1 col-start-1" />
+            <h3 className="text-4xl uppercase row-start-1 col-start-1 self-end mb-4 bg-clip-text text-transparent bg-gradient-to-b from-gray-400 to-gray-700">
+              desert
+            </h3>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="grid">
+            <img src={slide5} className="row-start-1 col-start-1" />
+            <h3 className="text-4xl uppercase row-start-1 col-start-1 self-end mb-4 bg-clip-text text-transparent bg-gradient-to-b from-gray-400 to-gray-700">
+              salad
+            </h3>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </section>
   );
 };
 
---
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+
+const SectionTitle = ({ heading, subHeading }) => {
+  return (
+    <div className="md:w-4/12 text-center my-12 mx-auto">
+      <p className="pb-2 capitalize bg-clip-text text-transparent bg-gradient-to-b from-yellow-400 to-yellow-700 italic">
+        --- {subHeading} ---
+      </p>
+      <h3 className="border-y-2 py-2 text-3xl uppercase bg-clip-text text-transparent bg-gradient-to-b from-gray-400 to-gray-700">
+        {heading}
+      </h3>
+    </div>
+  );
+};
+
+SectionTitle.propTypes = {
+  heading: PropTypes.string.isRequired,
+  subHeading: PropTypes.string,
+};
+
+export default SectionTitle;
