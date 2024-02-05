71eebd5c374e97d9a543e27529aaf5769c789382
mdmahbubekhuda <md.mahbub.e@gmail.com>
2024-02-05 17:22:26 +0600

    footer added using daisyUI

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Home/Shared/Footer/Footer";
 
 const Main = () => {
   return (
     <div>
       <Outlet />
+      <Footer />
     </div>
   );
 };
