c106dd3c51be87a28d01bbcd315e414b5f7885e1
mdmahbubekhuda <md.mahbub.e@gmail.com>
2024-02-09 16:16:21 +0600

    used custom hook to load menu data

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,22 +1,16 @@
-import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import MenuItem from "../../../components/MenuItem/MenuItem";
+import useMenu from "../../../hooks/useMenu";
 
 const PopularMenu = () => {
-  const [menu, setMenu] = useState([]);
-  useEffect(() => {
-    fetch("menu.json")
-      .then((res) => res.json())
-      .then((data) => {
-        const popularItems = data.filter((item) => item.category === "popular");
-        setMenu(popularItems);
-      });
-  }, []);
+  const [menu] = useMenu();
+  const popularItems = menu.filter((item) => item.category === "popular");
+
   return (
     <section className="mb-8">
       <SectionTitle heading={"from our menu"} subHeading={"popular items"} />
       <div className="grid md:grid-cols-2 gap-8">
-        {menu.map((item) => (
+        {popularItems.map((item) => (
           <MenuItem key={item._id} item={item} />
         ))}
       </div>
