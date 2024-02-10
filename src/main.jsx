9c49827efd2103396c9804bc31ae398c12585da9
mdmahbubekhuda <md.mahbub.e@gmail.com>
2024-02-11 02:24:35 +0600

    basic firebase setup done-env, authcontext, authprovider

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/Routes";
 
 import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./context/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <div className="max-w-screen-xl mx-auto">
-        <RouterProvider router={router} />
-      </div>
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <div className="max-w-screen-xl mx-auto">
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
