debfb710c95565b1c66ccc1b6b86d0195fc1e2c1
mdmahbubekhuda <md.mahbub.e@gmail.com>
2024-02-10 20:33:51 +0600

    DRY principle added to order page by removing repeated codes

--- src/Pages/Order/Order.jsx
+++ src/Pages/Order/Order.jsx
@@ -3,44 +3,51 @@ import coverImage from "../../assets/shop/banner2.jpg";
 
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useMenu from "../../hooks/useMenu";
 import FoodCard from "../../components/FoodCard/FoodCard";
 import { useParams } from "react-router-dom";
 
 const Order = () => {
   const [menu] = useMenu();
+  const [tabs, setTabs] = useState([]);
   const { category } = useParams();
+  const [tabIndex, setTabIndex] = useState(0);
 
-  const tabs = menu
-    .map((item) => item.category)
-    .filter((value, idx, currentValue) => currentValue.indexOf(value) === idx);
+  useEffect(() => {
+    const filteredTabNames = menu
+      .map((item) => item.category)
+      .filter((value, idx, currentValue) => currentValue.indexOf(value) === idx)
+      .sort();
 
-  const initialIndex = tabs.indexOf(category);
-  const [tabIndex, setTabIndex] = useState(initialIndex);
-  console.log(menu, category, initialIndex);
+    setTabs(filteredTabNames);
+    setTabIndex(filteredTabNames.indexOf(category));
+  }, [menu, category]);
 
   return (
     <div>
       <Cover coverImage={coverImage} coverTitle={"place order"} />
-      <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
-        <TabList>
+
+      {tabs.length && (
+        <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+          <TabList>
+            {tabs.map((tab, idx) => (
+              <Tab key={idx}>{tab.toUpperCase()}</Tab>
+            ))}
+          </TabList>
           {tabs.map((tab, idx) => (
-            <Tab key={idx}>{tab.toUpperCase()}</Tab>
+            <TabPanel key={idx}>
+              <div className="grid md:grid-cols-3 gap-12">
+                {menu
+                  .filter((item) => item.category === tab)
+                  .map((item) => (
+                    <FoodCard key={item._id} item={item} />
+                  ))}
+              </div>
+            </TabPanel>
           ))}
-        </TabList>
-        {tabs.map((tab, idx) => (
-          <TabPanel key={idx}>
-            <div className="grid md:grid-cols-3 gap-12">
-              {menu
-                .filter((item) => item.category === tab)
-                .map((item) => (
-                  <FoodCard key={item._id} item={item} />
-                ))}
-            </div>
-          </TabPanel>
-        ))}
-      </Tabs>
+        </Tabs>
+      )}
     </div>
   );
 };
